6fb51196d2285a6baba0c117f6edad483ab4ac35
chethack <techbuddy022@gmail.com>
2024-01-05 23:33:54 +0530

    added animation to routes form step

--- src/routes/login/+layout.svelte
+++ src/routes/login/+layout.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
     import { page } from "$app/stores";
+  import AnimatedRoute from "$lib/components/AnimatedRoute.svelte";
 </script>
   
-  <nav class="flex justify-center my-6">
+<nav class="flex justify-center my-6">
     <ul class="steps">
       <a href="/login" class="step step-primary">Sign In</a>
       <a
@@ -18,10 +19,13 @@
         Upload Photo
       </a>
     </ul>
-  </nav>
-  
+</nav>
+
+<AnimatedRoute>
   <main class="card w-4/6 bg-neutral text-neutral-content mx-auto">
     <div class="card-body items-center text-center">
       <slot />
     </div>
   </main>
+  
+</AnimatedRoute>
