d1a073769a972bc0d00ba369ed74a1c53e83ec89
chethack <techbuddy022@gmail.com>
2024-01-07 21:37:16 +0530

    added user auth store

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,20 +1,45 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { doc, getFirestore, onSnapshot } from "firebase/firestore";
+import { getAuth, onAuthStateChanged, type User } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+import { writable, type Readable, derived } from "svelte/store";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyARsVGzNdFoNqx7hkC6Ldri8NeSEyI07xE",
-  authDomain: "svelte-course-17e03.firebaseapp.com",
-  projectId: "svelte-course-17e03",
-  storageBucket: "svelte-course-17e03.appspot.com",
-  messagingSenderId: "195311295545",
-  appId: "1:195311295545:web:17c48f867771550836c1e8",
-  measurementId: "G-6K003R5S90"
+
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
 export const auth = getAuth();
-export const storage  = getStorage();
+export const storage = getStorage();
+
+
+/**
+ * @returns a store with the current firebase user
+ */
+function userStore() {
+  let unsubscribe: () => void;
+
+  if (!auth || !globalThis.window) {
+    console.warn('Auth is not initialized or not in browser');
+    const { subscribe } = writable<User | null>(null);
+    return {
+      subscribe,
+    }
+  }
+
+  const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
+    unsubscribe = onAuthStateChanged(auth, (user) => {
+      set(user);
+    });
+
+    return () => unsubscribe();
+  });
+
+  return {
+    subscribe,
+  };
+}
+
+export const user = userStore();
