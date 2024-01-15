ec70bff0824ecbb0ef2e172e70bfefd009eff57b
chetanyb <techbuddy022@gmail.com>
2024-01-15 21:50:18 +0530

    Refactor username input and availability check

--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -6,9 +6,13 @@
     let username = "";
     let loading = false;
     let isAvailable = false;
-  
-    
     let debounceTimer: NodeJS.Timeout;
+
+    const re = /^(?=[a-zA-Z0-9._]{3,16}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+  
+  $: isValid = username?.length > 2 && username.length < 16 && re.test(username);
+  $: isTouched = username.length > 0;
+  $: isTaken = isValid && !isAvailable && !loading
   
     async function checkAvailability() {
       isAvailable = false;
@@ -57,22 +61,40 @@
   </script>
   
   
-  
   <AuthCheck>
-      <h2>Username</h2>
-      <form class="w-2/5" on:submit|preventDefault={confirmUsername}>
-          <input
-            type="text"
-            placeholder="Username"
-            class="input w-full"
-            bind:value={username}
-            on:input={checkAvailability}
-          />
-  
-          <p>Is available? {isAvailable}</p>
-  
+    <form class="w-2/5" on:submit|preventDefault={confirmUsername}>
+      <input
+        type="text"
+        placeholder="Username"
+        class="input w-full"
+        bind:value={username}
+        on:input={checkAvailability}
+        class:input-error={(!isValid && isTouched)}
+        class:input-warning={isTaken}
+        class:input-success={isAvailable && isValid && !loading}
+      />
+      <div class="my-4 min-h-16 px-8 w-full">
+        {#if loading}
+          <p class="text-secondary">Checking availability of @{username}...</p>
+        {/if}
+    
+        {#if !isValid && isTouched}
+          <p class="text-error text-sm">
+            must be 3-16 characters long, alphanumeric only
+          </p>
+        {/if}
+    
+        {#if isValid && !isAvailable && !loading}
+          <p class="text-warning text-sm">
+            @{username} is not available
+          </p>
+        {/if}
+    
+        {#if isAvailable}
           <button class="btn btn-success">Confirm username @{username} </button>
-  
-        </form>
-  
-  </AuthCheck>
+        {/if}
+      </div>
+    </form>
+    
+    
+    </AuthCheck>
